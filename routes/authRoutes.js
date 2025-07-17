e7729840fbd6ff206d7c877e664e013fc55a8eb3
mani1234567sk <abdulrehmn78976@gmail.com>
2025-07-18 00:37:29 +0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,7 @@ const { authenticateToken, isAdmin } = require('../middleware/authMiddleware');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/users', authenticateToken, isAdmin, authController.getAllUsers);
+router.put('/users/:id', authenticateToken, isAdmin, authController.updateUser);
 router.delete('/users/:id', authenticateToken, isAdmin, authController.deleteUser);
 router.put('/profile', authenticateToken, authController.updateProfile);
 
