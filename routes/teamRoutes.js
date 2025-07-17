7420ac4365e35385ccb320f4dfe094cecf7726f1
mani1234567sk <abdulrehmn78976@gmail.com>
2025-07-18 00:38:37 +0500

    Update teamRoutes.js

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -5,6 +5,7 @@ const { authenticateToken, isAdmin } = require('../middleware/authMiddleware');
 
 router.get('/', teamController.getTeams);
 router.post('/', authenticateToken, teamController.createTeam);
+router.put('/:id', authenticateToken, isAdmin, teamController.updateTeam);
 router.delete('/:id', authenticateToken, isAdmin, teamController.deleteTeam);
 
 module.exports = router;
