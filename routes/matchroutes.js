6705a4b0086ef4276a1b73c6b46a6ffb61d07d54
mani1234567sk <abdulrehmn78976@gmail.com>
2025-07-24 15:43:48 +0500

    Create matchroutes.js

---
+++ routes/matchroutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const matchController = require('../controllers/matchController');
+const { authenticateToken } = require('../middleware/authMiddleware');
+
+router.get('/', matchController.getMatches);
+router.post('/', authenticateToken, matchController.createMatch);
+router.post('/:id/join', authenticateToken, matchController.joinMatch);
+router.put('/:id', authenticateToken, matchController.updateMatch);
+router.delete('/:id', authenticateToken, matchController.deleteMatch);
+
+module.exports = router;
