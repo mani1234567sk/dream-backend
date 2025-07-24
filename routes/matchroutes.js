4d6eba2aa3ababe7d923f22f49f04071ea7ddc5d
mani1234567sk <abdulrehmn78976@gmail.com>
2025-07-24 16:22:26 +0500

    Update matchroutes.js

--- routes/matchroutes.js
+++ routes/matchroutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const matchController = require('../controllers/matchController');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const { authenticateToken, isAdmin } = require('../middleware/authMiddleware');
 
 // Public route for getting matches (no auth required for viewing)
 router.get('/', matchController.getMatches);
-router.post('/', authenticateToken, matchController.createMatch);
+router.post('/', authenticateToken, isAdmin, matchController.createMatch);
 router.post('/:id/join', authenticateToken, matchController.joinMatch);
-router.put('/:id', authenticateToken, matchController.updateMatch);
-router.delete('/:id', authenticateToken, matchController.deleteMatch);
+router.put('/:id', authenticateToken, isAdmin, matchController.updateMatch);
+router.delete('/:id', authenticateToken, isAdmin, matchController.deleteMatch);
 
 module.exports = router;
